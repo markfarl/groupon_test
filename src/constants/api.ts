8dab69723a692098d469ebb1bb65f84c986486e1
markfarl <markfarl@gmail.com>
2024-11-14 23:33:55 +0000

    updated endpoints

--- src/constants/api.ts
+++ src/constants/api.ts
@@ -1,8 +1,9 @@
 // AWS server
-//const SERVER_URL = "http://ec2-13-61-12-222.eu-north-1.compute.amazonaws.com:443/search?"
+// const SERVER_URL = "http://ec2-13-61-12-222.eu-north-1.compute.amazonaws.com:443/search?"
 
 // GROK forwarding
-const SERVER_URL = "https://d437744729f7.ngrok.app/search?"
+const SERVER_URL = "https://werewolf-neat-certainly.ngrok-free.app/search?"
+
 // Local server
 // const SERVER_URL = "http://127.0.0.1:4000/search?"
 
