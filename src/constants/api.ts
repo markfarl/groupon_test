7f8fee6e81b91f13a042aa5ae08528fd54aa711d
markfarl <markfarl@gmail.com>
2024-11-14 18:58:02 +0000

    updated API

--- src/constants/api.ts
+++ src/constants/api.ts
@@ -1,7 +1,7 @@
 // AWS server
-//const SERVER_URL = "http://13.51.199.166:443/search?"
+const SERVER_URL = "http://ec2-13-61-12-222.eu-north-1.compute.amazonaws.com:443/search?"
 
 // Local server
- const SERVER_URL = "http://127.0.0.1:4000/search?"
+// const SERVER_URL = "http://127.0.0.1:4000/search?"
 
 export { SERVER_URL  }
